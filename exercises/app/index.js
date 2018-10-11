f97ef609404760001cc6e089eef987209bcacbaf
Scott Moss <willscottmoss@gmail.com>
2018-10-11 08:29:28 -0500

    chore

--- exercises/app/index.js
+++ exercises/app/index.js
@@ -11,14 +11,19 @@ app.use(json())
 
 app.get('/todo/:id', async (req, res) => {
   const todoId = req.params.id
+  const todo = await Todo.findById(id).exec()
+  res.status(200).json(todo)
 })
 
 app.get('/todos', async (req, res) => {
-  
+  const todos = await Todo.find({}).lean().exec()
+  res.status(200).json(todos)
 })
 
 app.post('/todo', async () => {
   const todoToCreate = req.body.todo
+  const todo = await Todo.create(todoToCreate)
+  res.status(201).json(todo.toJSON())
 })
 
 connect('mongodb://localhost:27017/intro-to-mongodb')
