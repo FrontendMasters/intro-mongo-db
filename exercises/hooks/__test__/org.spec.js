c437090b7d21b48a5b16ce1b29fe293db9e7a74f
Cristian Gabbanini <cristian.gabbanini@gmail.com>
2019-04-18 15:19:22 +0200

    Adds testing for the organization avatar url

--- exercises/hooks/__test__/org.spec.js
+++ exercises/hooks/__test__/org.spec.js
@@ -1,17 +1,23 @@
-const Project = require('../project')
-const Org = require('../org')
-const mongoose = require('mongoose')
+const Project = require('../project');
+const Org = require('../org');
+const mongoose = require('mongoose');
+const cdnUrl = 'https://cdn.adminapp.com';
 
 describe('Org model', () => {
-  test('removes projects when org is remove', async () => {
-    const org = await Org.create({name: 'org'})
+  test('removes projects when org is removed', async () => {
+    const org = await Org.create({ name: 'org' });
     await Project.create([
-      {name: 'project1', org: org.id},
-      {name: 'project', org: org.id}
-    ])
-    
-    await org.remove()
-    const matchedProjects = await Project.find({org: org._id}).exec()
-    expect(matchedProjects).toHaveLength(0)
-  })
-})
+      { name: 'project1', org: org.id },
+      { name: 'project', org: org.id }
+    ]);
+
+    await org.remove();
+    const matchedProjects = await Project.find({ org: org._id }).exec();
+    expect(matchedProjects).toHaveLength(0);
+  });
+
+  test('has the avatar virtual getter', async () => {
+    const org = await Org.create({ name: 'org' });
+    expect(org.avatar).not.toBe(`${cdnUrl}/${org._id}`);
+  });
+});
