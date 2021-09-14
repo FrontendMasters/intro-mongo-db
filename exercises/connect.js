8fab8f06410bbb600cc7c0fde40c96883e0461cf
Axhar85 <azhararshad_ntu@yahoo.com>
2021-09-14 14:00:38 +0200

    run the mongodb tests

--- exercises/connect.js
+++ exercises/connect.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-const connect = (url) => Promise.reject()
+const connect = (url) => mongoose.connect(url, {
+    useNewUrlParser: true
+})
 
 module.exports = connect
--- exercises/models/user.js
+++ exercises/models/user.js
@@ -2,16 +2,22 @@ const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
   firstName: {
-    type: String
+    type: String,
+    required: true
+
   },
   lastName: {
-    type: String
+    type: String,
+    required: true
   },
   email: {
-    type: String
+    type: String,
+    required: true,
+    unique: true
   },
   betaUser: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   birthDate: Date,
   pets: [{type: String}],
