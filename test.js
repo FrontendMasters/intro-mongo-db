c88060d47a91abfc50e8fa89bd2421a9bde1bfa4
Axhar85 <azhararshad_ntu@yahoo.com>
2021-09-14 14:20:32 +0200

    created Test file

---
+++ test.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const connect = () => {
+    return mongoose.connect('mongodb://localhost:27017whatever')
+}
+
+//creating scheems
+
+const student = new mongoose.Schema ( {
+    firstName: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    faveFood: [{type: Strings}],
+
+    info: {
+        school: {
+            type: String
+        },
+        shoeSize: {
+            type: Number
+        }
+
+    }
+})
+
+const Student = mongoose.model('student', student)
+
+//connecting with db
+
+connect()
+.then(async connection => {
+    const student= await Student.create({firstName: 'Tim'})
+    const found = await student.find({firstName:'Thi'})
+    const foundById= await student.findById('asygcgk')
+    const updated= await student.findByIdUpdate('fytdydtd',{})
+    console.log(student)
+})
+.catch(e => console.error(e))
